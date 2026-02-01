e3c89af074ae93f347912f4aa543e40a00faaa33
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-02-01 15:42:33 +0100

    Refactor

--- output/live-events/DataDomeEventsTracking.js
+++ output/live-events/DataDomeEventsTracking.js
@@ -1,114 +1,130 @@
-var r = require("../http/DataDomeRequest");
-var i = require("../common/DataDomeTools");
-function c(n) {
+var DataDomeRequest = require("../http/DataDomeRequest");
+var DataDomeTools = require("../common/DataDomeTools");
+
+// wrap a function in a try/catch that returns null on error
+function safeCaller(fn) {
   return function () {
     try {
-      return n.apply(this, arguments);
-    } catch (n) {
+      return fn.apply(this, arguments);
+    } catch (err) {
       return null;
     }
   };
 }
-function o(n) {
-  return function (n) {
-    var e = 16777619;
-    var t = 2166136261;
-    for (var r = 0; r < n.length; r++) {
-      t = (t ^= n.charCodeAt(r)) * e >>> 0;
+
+// FNV-1a hash of event counters (mousemove, touchmove, scroll, click, keydown, keyup)
+function computeUISignatureHash(counters) {
+  return function (str) {
+    var prime = 16777619;
+    var hash = 2166136261;
+    for (var i = 0; i < str.length; i++) {
+      hash = (hash ^= str.charCodeAt(i)) * prime >>> 0;
     }
-    return t;
-  }([Math.ceil(n.mousemove / 10), Math.ceil(n.touchmove / 10), n.scroll, n.click, n.keydown > 0 ? 1 : 0, n.keyup > 0 ? 1 : 0].join("_"));
+    return hash;
+  }([Math.ceil(counters.mousemove / 10), Math.ceil(counters.touchmove / 10), counters.scroll, counters.click, counters.keydown > 0 ? 1 : 0, counters.keyup > 0 ? 1 : 0].join("_"));
 }
-function a(n, e) {
-  if (!n || n.length == 0) {
+
+// compute percentile value from a sorted array (e.g. 50 = median)
+function percentile(arr, pct) {
+  if (!arr || arr.length == 0) {
     return null;
   }
-  var t = n.sort(function (n, e) {
-    return n - e;
+  var sorted = arr.sort(function (a, b) {
+    return a - b;
   });
-  var r = (t.length - 1) * e / 100;
-  var i = Math.floor(r);
-  if (t[i + 1] !== undefined) {
-    var c = r - i;
-    return t[i] + c * (t[i + 1] - t[i]);
+  var rank = (sorted.length - 1) * pct / 100;
+  var lowerIndex = Math.floor(rank);
+  if (sorted[lowerIndex + 1] !== undefined) {
+    var fraction = rank - lowerIndex;
+    return sorted[lowerIndex] + fraction * (sorted[lowerIndex + 1] - sorted[lowerIndex]);
   }
-  return t[i];
+  return sorted[lowerIndex];
 }
-function s(n, e, t, r) {
-  var i = t - n;
-  var c = r - e;
-  var o = Math.acos(i / Math.sqrt(i * i + c * c));
-  if (c < 0) {
-    return -o;
+
+// compute angle in radians between two points
+function computeAngle(x1, y1, x2, y2) {
+  var dx = x2 - x1;
+  var dy = y2 - y1;
+  var angle = Math.acos(dx / Math.sqrt(dx * dx + dy * dy));
+  if (dy < 0) {
+    return -angle;
   } else {
-    return o;
+    return angle;
   }
 }
-function f(n) {
-  if (!n || n.length == 0) {
+
+// arithmetic mean of an array
+function mean(arr) {
+  if (!arr || arr.length == 0) {
     return null;
   }
-  var e = 0;
-  for (var t = 0; t < n.length; t++) {
-    e += n[t];
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
-  return e / n.length;
+  return sum / arr.length;
 }
-function u(n, e) {
-  if (!n || n.length == 0) {
+
+// standard deviation of an array given a precomputed mean
+function stddev(arr, avg) {
+  if (!arr || arr.length == 0) {
     return null;
   }
-  var t = 0;
-  for (var r = 0; r < n.length; r++) {
-    var i = e - n[r];
-    t += Math.pow(i, 2);
+  var sumSquares = 0;
+  for (var i = 0; i < arr.length; i++) {
+    var diff = avg - arr[i];
+    sumSquares += Math.pow(diff, 2);
   }
-  var c = t / n.length;
-  return Math.sqrt(c);
+  var variance = sumSquares / arr.length;
+  return Math.sqrt(variance);
 }
-function h(n) {
-  this.keysAnalyzer = new w();
-  this.mouseAnalyzer = new l();
-  var e = false;
-  this._eventIsValid = function (n) {
-    if (n.isTrusted && !n.repeat) {
-      var e = performance.now();
-      if (n.timeStamp > 0 && n.timeStamp > e - 5000 && n.timeStamp < e) {
+
+// orchestrates mouse and keyboard analyzers, validates events, dispatches to analyzers
+function EventStats(wrapper) {
+  this.keysAnalyzer = new KeysAnalyzer();
+  this.mouseAnalyzer = new MouseAnalyzer();
+  var firstMouseMoveSent = false;
+  this._eventIsValid = function (event) {
+    if (event.isTrusted && !event.repeat) {
+      var now = performance.now();
+      if (event.timeStamp > 0 && event.timeStamp > now - 5000 && event.timeStamp < now) {
         return true;
       }
     }
     return false;
   };
-  this.handleEvent = function (t) {
-    if (this._eventIsValid(t)) {
-      switch (t.type) {
+  this.handleEvent = function (event) {
+    if (this._eventIsValid(event)) {
+      switch (event.type) {
         case "mousemove":
-          if (!e) {
-            e = true;
-            n.i("m_fmi", t.pageY == t.screenY && t.pageX == t.screenX);
+          if (!firstMouseMoveSent) {
+            firstMouseMoveSent = true;
+            wrapper.i("m_fmi", event.pageY == event.screenY && event.pageX == event.screenX);
           }
-          this.mouseAnalyzer._handleMouseMove(t);
+          this.mouseAnalyzer._handleMouseMove(event);
           break;
         case "keydown":
         case "keyup":
-          this.keysAnalyzer.recordKeyEvent(t);
+          this.keysAnalyzer.recordKeyEvent(event);
       }
     }
   };
   this.buildAndStoreSignals = function () {
     try {
-      var e = this.mouseAnalyzer.computeSignals();
-      var t = this.keysAnalyzer.computeSignals();
-      for (var r in e) {
-        n.i(r, e[r]);
+      var mouseSignals = this.mouseAnalyzer.computeSignals();
+      var keySignals = this.keysAnalyzer.computeSignals();
+      for (var key in mouseSignals) {
+        wrapper.i(key, mouseSignals[key]);
       }
-      for (var i in t) {
-        n.i(i, t[i]);
+      for (var key2 in keySignals) {
+        wrapper.i(key2, keySignals[key2]);
       }
-    } catch (n) {}
+    } catch (err) {}
   };
 }
-function l() {
+
+// analyzes mouse movement strokes: timestamps, distances, angles
+function MouseAnalyzer() {
   this._lastMouseMoveEvent = null;
   this._currentStrokeEvents = [];
   this._sigmas = [];
@@ -118,232 +134,234 @@ function l() {
   this._endAngles = [];
   this._consumeStroke = function () {
     try {
-      var n = this._currentStrokeEvents.length;
-      if (n > 1) {
-        var e = 0;
-        var t = 0;
-        for (var r = 0; r < n; r++) {
-          var i = Math.log(this._currentStrokeEvents[r].timeStamp);
-          e += i;
-          t += i * i;
+      var count = this._currentStrokeEvents.length;
+      if (count > 1) {
+        var sumLog = 0;
+        var sumLogSquared = 0;
+        for (var i = 0; i < count; i++) {
+          var logTs = Math.log(this._currentStrokeEvents[i].timeStamp);
+          sumLog += logTs;
+          sumLogSquared += logTs * logTs;
         }
-        this._sigmas.push(Math.sqrt((n * t - e * e) / n * (n - 1)) / 1000);
-        this._mus.push(e / n);
-        var c = this._currentStrokeEvents[0];
-        var o = this._currentStrokeEvents[n - 1];
-        this._dists.push((w = c.clientX, d = c.clientY, v = o.clientX, b = o.clientY, y = v - w, p = b - d, Math.sqrt(y * y + p * p)));
-        var a = n < 4 ? n - 1 : 3;
-        var f = this._currentStrokeEvents[a];
-        var u = this._currentStrokeEvents[n - a - 1];
-        var h = s(c.clientX, c.clientY, f.clientX, f.clientY);
-        var l = s(o.clientX, o.clientY, u.clientX, u.clientY);
-        this._startAngles.push(h);
-        this._endAngles.push(l);
+        this._sigmas.push(Math.sqrt((count * sumLogSquared - sumLog * sumLog) / count * (count - 1)) / 1000);
+        this._mus.push(sumLog / count);
+        var firstEvent = this._currentStrokeEvents[0];
+        var lastEvent = this._currentStrokeEvents[count - 1];
+        this._dists.push((dx = firstEvent.clientX, dy = firstEvent.clientY, dx2 = lastEvent.clientX, dy2 = lastEvent.clientY, ddx = dx2 - dx, ddy = dy2 - dy, Math.sqrt(ddx * ddx + ddy * ddy)));
+        var angleOffset = count < 4 ? count - 1 : 3;
+        var nearStartEvent = this._currentStrokeEvents[angleOffset];
+        var nearEndEvent = this._currentStrokeEvents[count - angleOffset - 1];
+        var startAngle = computeAngle(firstEvent.clientX, firstEvent.clientY, nearStartEvent.clientX, nearStartEvent.clientY);
+        var endAngle = computeAngle(lastEvent.clientX, lastEvent.clientY, nearEndEvent.clientX, nearEndEvent.clientY);
+        this._startAngles.push(startAngle);
+        this._endAngles.push(endAngle);
       }
       this._currentStrokeEvents = [];
-    } catch (n) {}
-    var w;
-    var d;
-    var v;
-    var b;
-    var y;
-    var p;
+    } catch (err) {}
+    var dx;
+    var dy;
+    var dx2;
+    var dy2;
+    var ddx;
+    var ddy;
   };
-  this._handleMouseMove = function (n) {
+  this._handleMouseMove = function (event) {
     if (this._lastMouseMoveEvent) {
-      if (n.timeStamp - this._lastMouseMoveEvent.timeStamp > 499) {
+      if (event.timeStamp - this._lastMouseMoveEvent.timeStamp > 499) {
         this._consumeStroke();
       }
     }
-    this._currentStrokeEvents.push(n);
-    this._lastMouseMoveEvent = n;
+    this._currentStrokeEvents.push(event);
+    this._lastMouseMoveEvent = event;
   };
   this.computeSignals = function () {
     try {
       this._consumeStroke();
       return {
-        es_sigmdn: c(a)(this._sigmas, 50),
-        es_mumdn: c(a)(this._mus, 50),
-        es_distmdn: c(a)(this._dists, 50),
-        es_angsmdn: c(a)(this._startAngles, 50),
-        es_angemdn: c(a)(this._endAngles, 50)
+        es_sigmdn: safeCaller(percentile)(this._sigmas, 50),
+        es_mumdn: safeCaller(percentile)(this._mus, 50),
+        es_distmdn: safeCaller(percentile)(this._dists, 50),
+        es_angsmdn: safeCaller(percentile)(this._startAngles, 50),
+        es_angemdn: safeCaller(percentile)(this._endAngles, 50)
       };
-    } catch (n) {
+    } catch (err) {
       return {};
     }
   };
 }
-function w() {
+
+// analyzes keyboard events: hold time, press-to-press, release-to-release, inter-key timing
+function KeysAnalyzer() {
   this._keyEvents = [];
   this.keydowns = 0;
   this.keyups = 0;
-  this.recordKeyEvent = function (n) {
+  this.recordKeyEvent = function (event) {
     try {
-      if (n && n instanceof KeyboardEvent && (n.type === "keydown" || n.type === "keyup")) {
+      if (event && event instanceof KeyboardEvent && (event.type === "keydown" || event.type === "keyup")) {
         this._keyEvents.push({
-          ts: n.timeStamp,
-          key: n.key,
-          type: n.type
+          ts: event.timeStamp,
+          key: event.key,
+          type: event.type
         });
       }
-    } catch (n) {}
+    } catch (err) {}
   };
-  this._getSequenceWindows = function (n, e) {
-    var t = [];
-    for (var r = 0; r < n.length - e + 1; r++) {
-      t.push(n.slice(r, r + e));
+  this._getSequenceWindows = function (arr, windowSize) {
+    var windows = [];
+    for (var i = 0; i < arr.length - windowSize + 1; i++) {
+      windows.push(arr.slice(i, i + windowSize));
     }
-    return t;
+    return windows;
   };
   this.computeSignals = function () {
     try {
-      var n = [];
-      var e = [];
-      var t = [];
-      var r = [];
-      var i = null;
-      var o = null;
-      var a = {};
-      var s = [];
-      var h = new window.Set();
-      for (var l = 0; l < this._keyEvents.length; l++) {
-        var w = this._keyEvents[l];
-        if (w.type === "keydown") {
+      var holdTimes = [];
+      var pressToPress = [];
+      var releaseToRelease = [];
+      var interKeyTimes = [];
+      var lastKeydown = null;
+      var lastKeyup = null;
+      var activeKeys = {};
+      var keyPairs = [];
+      var pairedIndices = new window.Set();
+      for (var i = 0; i < this._keyEvents.length; i++) {
+        var evt = this._keyEvents[i];
+        if (evt.type === "keydown") {
           this.keydowns++;
-          a[w.key] = w;
-          if (i) {
-            e.push(w.ts - i.ts);
+          activeKeys[evt.key] = evt;
+          if (lastKeydown) {
+            pressToPress.push(evt.ts - lastKeydown.ts);
           }
-          i = w;
-        } else if (w.type === "keyup") {
+          lastKeydown = evt;
+        } else if (evt.type === "keyup") {
           this.keyups++;
-          if (a[w.key]) {
-            var d = a[w.key];
-            a[w.key] = null;
-            n.push(w.ts - d.ts);
+          if (activeKeys[evt.key]) {
+            var downEvt = activeKeys[evt.key];
+            activeKeys[evt.key] = null;
+            holdTimes.push(evt.ts - downEvt.ts);
           }
-          if (o) {
-            t.push(w.ts - o.ts);
+          if (lastKeyup) {
+            releaseToRelease.push(evt.ts - lastKeyup.ts);
           }
-          o = w;
+          lastKeyup = evt;
         }
-        if (!h.has(l)) {
-          for (var v = l + 1; v < this._keyEvents.length; v++) {
-            var b = this._keyEvents[v];
-            if (w.key === b.key) {
-              s.push([w, b]);
-              h.add(l);
-              h.add(v);
+        if (!pairedIndices.has(i)) {
+          for (var j = i + 1; j < this._keyEvents.length; j++) {
+            var other = this._keyEvents[j];
+            if (evt.key === other.key) {
+              keyPairs.push([evt, other]);
+              pairedIndices.add(i);
+              pairedIndices.add(j);
               break;
             }
           }
         }
       }
-      for (var y = this._getSequenceWindows(s, 2), p = 0; p < y.length; p++) {
-        var m = y[p][0];
-        var F = y[p][1];
-        r.push(F[0].ts - m[1].ts);
+      for (var pairWindows = this._getSequenceWindows(keyPairs, 2), k = 0; k < pairWindows.length; k++) {
+        var firstPair = pairWindows[k][0];
+        var secondPair = pairWindows[k][1];
+        interKeyTimes.push(secondPair[0].ts - firstPair[1].ts);
       }
-      var L = c(f)(n);
-      var U = c(f)(e);
-      var A = c(f)(t);
-      var Q = c(f)(r);
+      var holdMean = safeCaller(mean)(holdTimes);
+      var pressMean = safeCaller(mean)(pressToPress);
+      var releaseMean = safeCaller(mean)(releaseToRelease);
+      var interKeyMean = safeCaller(mean)(interKeyTimes);
       return {
-        k_hA: L,
-        k_hSD: c(u)(n, L),
-        k_pA: U,
-        k_pSD: c(u)(e, U),
-        k_rA: A,
-        k_rSD: c(u)(t, A),
-        k_ikA: Q,
-        k_ikSD: c(u)(r, Q),
+        k_hA: holdMean,
+        k_hSD: safeCaller(stddev)(holdTimes, holdMean),
+        k_pA: pressMean,
+        k_pSD: safeCaller(stddev)(pressToPress, pressMean),
+        k_rA: releaseMean,
+        k_rSD: safeCaller(stddev)(releaseToRelease, releaseMean),
+        k_ikA: interKeyMean,
+        k_ikSD: safeCaller(stddev)(interKeyTimes, interKeyMean),
         k_kdc: this.keydowns,
         k_kuc: this.keyups
       };
-    } catch (n) {
+    } catch (err) {
       return {};
     }
   };
 }
-module.exports.EventStats = h;
-module.exports.DataDomeEventsTracking = function (n, e) {
-  var t = 10000;
-  var c = true;
-  var a = "le";
-  if (e) {
-    a = "fm";
+module.exports.EventStats = EventStats;
+module.exports.DataDomeEventsTracking = function (wrapper, isFormMode) {
+  var SEND_DELAY = 10000;
+  var useCapture = true;
+  var jsType = "le";
+  if (isFormMode) {
+    jsType = "fm";
   }
-  var s;
-  var f = new r(a);
-  var u = new i();
-  var l = new h(n);
-  var w = false;
-  var d = null;
-  var v = false;
-  var b = false;
-  var y = ["mousemove", "click", "scroll", "touchstart", "touchend", "touchmove", "keydown", "keyup"];
-  var p = function () {
-    var n = {};
-    for (var e = 0; e < y.length; e++) {
-      n[y[e]] = 0;
+  var rafId;
+  var request = new DataDomeRequest(jsType);
+  var tools = new DataDomeTools();
+  var eventStats = new EventStats(wrapper);
+  var hasSent = false;
+  var sendTimer = null;
+  var isAnimationFrameFired = false;
+  var hasCollected = false;
+  var EVENT_TYPES = ["mousemove", "click", "scroll", "touchstart", "touchend", "touchmove", "keydown", "keyup"];
+  var eventCounters = function () {
+    var counters = {};
+    for (var i = 0; i < EVENT_TYPES.length; i++) {
+      counters[EVENT_TYPES[i]] = 0;
     }
-    return n;
+    return counters;
   }();
-  function m(n) {
-    w = true;
-    if (!e) {
+  function onEvent(event) {
+    hasSent = true;
+    if (!isFormMode) {
       (function () {
-        if (d != null || !v) {
+        if (sendTimer != null || !isAnimationFrameFired) {
           return;
         }
-        d = setTimeout(function () {
-          F(true);
-        }, t);
+        sendTimer = setTimeout(function () {
+          sendData(true);
+        }, SEND_DELAY);
       })();
     }
-    p[n.type]++;
-    l.handleEvent(n);
+    eventCounters[event.type]++;
+    eventStats.handleEvent(event);
   }
-  function F(e) {
-    if (!b && w && window.dataDomeOptions) {
-      b = true;
-      l.buildAndStoreSignals();
-      n.i("m_s_c", p.scroll);
-      n.i("m_m_c", p.mousemove);
-      n.i("m_c_c", p.click);
-      n.i("m_cm_r", p.mousemove === 0 ? -1 : p.click / p.mousemove);
-      n.i("m_ms_r", p.scroll === 0 ? -1 : p.mousemove / p.scroll);
+  function sendData(needsResponse) {
+    if (!hasCollected && hasSent && window.dataDomeOptions) {
+      hasCollected = true;
+      eventStats.buildAndStoreSignals();
+      wrapper.i("m_s_c", eventCounters.scroll);
+      wrapper.i("m_m_c", eventCounters.mousemove);
+      wrapper.i("m_c_c", eventCounters.click);
+      wrapper.i("m_cm_r", eventCounters.mousemove === 0 ? -1 : eventCounters.click / eventCounters.mousemove);
+      wrapper.i("m_ms_r", eventCounters.scroll === 0 ? -1 : eventCounters.mousemove / eventCounters.scroll);
       try {
-        var t = o(p);
-        n.i("uish", String(t));
-      } catch (n) {}
-      f.requestApi(window.ddjskey, n, p, window.dataDomeOptions.patternToRemoveFromReferrerUrl, e, window.dataDomeOptions.ddResponsePage);
+        var hash = computeUISignatureHash(eventCounters);
+        wrapper.i("uish", String(hash));
+      } catch (err) {}
+      request.requestApi(window.ddjskey, wrapper, eventCounters, window.dataDomeOptions.patternToRemoveFromReferrerUrl, needsResponse, window.dataDomeOptions.ddResponsePage);
       (function () {
-        for (var n = 0; n < y.length; n++) {
-          u.removeEventListener(document, y[n], m, c);
+        for (var i = 0; i < EVENT_TYPES.length; i++) {
+          tools.removeEventListener(document, EVENT_TYPES[i], onEvent, useCapture);
         }
       })();
     }
   }
   this.process = function () {
     (function () {
-      for (var n = 0; n < y.length; n++) {
-        u.addEventListener(document, y[n], m, c);
+      for (var i = 0; i < EVENT_TYPES.length; i++) {
+        tools.addEventListener(document, EVENT_TYPES[i], onEvent, useCapture);
       }
     })();
-    s = window.requestAnimationFrame(function (n) {
-      v = true;
+    rafId = window.requestAnimationFrame(function (timestamp) {
+      isAnimationFrameFired = true;
     });
-    if (!e) {
-      u.addEventListener(window, "onpagehide" in window ? "pagehide" : "beforeunload", function () {
-        clearTimeout(d);
-        window.cancelAnimationFrame(s);
-        F(false);
+    if (!isFormMode) {
+      tools.addEventListener(window, "onpagehide" in window ? "pagehide" : "beforeunload", function () {
+        clearTimeout(sendTimer);
+        window.cancelAnimationFrame(rafId);
+        sendData(false);
       });
     }
   };
   this.collect = function () {
-    F(true);
+    sendData(true);
   };
 };
-module.exports.nech = o;
+module.exports.nech = computeUISignatureHash;
