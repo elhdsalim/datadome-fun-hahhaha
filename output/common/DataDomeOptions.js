a92d3eb064174bdb58bc426819835a65fc33e5fa
slmhd <salim.elhadad.etu@univ-lille.fr>
2026-02-01 09:41:07 +0100

    simplified the code / added explanations

--- output/common/DataDomeOptions.js
+++ output/common/DataDomeOptions.js
@@ -1,162 +1,130 @@
+/**
+ * This is not part of the detection system, it configures the behavior of other modules of the tag (network, listeners, events, captcha, cookies…)
+ */
+
 module.exports = function () {
-  this.endpoint = "https://api-js.datadome.co/js/";
-  this.version = "5.2.0";
-  this.ajaxListenerPath = null;
-  this.ajaxListenerPathExclusion = null;
-  this.customParam = null;
-  this.exposeCaptchaFunction = false;
-  this.abortAsyncOnChallengeDisplay = true;
-  this.patternToRemoveFromReferrerUrl = null;
-  this.eventsTrackingEnabled = true;
-  this.overrideAbortFetch = true;
-  this.ddResponsePage = "origin";
-  this.isSalesforce = false;
-  this.disableAutoRefreshOnCaptchaPassed = false;
-  this.enableTagEvents = false;
-  this.withCredentials = false;
-  this.overrideCookieDomain = false;
-  this.dryRun = [];
-  this.sessionByHeader = false;
-  this.ddCookieSessionName = "ddSession";
-  this.enableServiceWorkerPlugin = false;
-  this.deferSignals = false;
-  this.replayAfterChallenge = false;
-  this.enableCookieDomainFallback = false;
-  this.challengeLanguage = null;
-  this.check = function (n) {
-    if (!n || typeof n != "object") {
-      n = {};
+  // === default values ===
+  this.endpoint = "https://api-js.datadome.co/js/"; // endpoint where the tag sends fingerprint signals
+  this.version = "5.2.0"; // latest version of tags.js
+  this.ajaxListenerPath = null; // paths/urls to watch => if true, datadome watches all requests to the current domain
+  this.ajaxListenerPathExclusion = null; // urls to exclude from the watch
+  this.customParam = null; // custom parameter to send with each datadome request
+  this.exposeCaptchaFunction = false; // if true => exposes window.displayDataDomeCaptchaPage() to manually show the captcha
+  this.abortAsyncOnChallengeDisplay = true; // if true => cancel async requests when a challenge is displayed
+  this.patternToRemoveFromReferrerUrl = null; // regex to clean the referrer url
+  this.eventsTrackingEnabled = true; // enable user events (mouse movements, mouse clicks...etc...)
+  this.overrideAbortFetch = true; // removes AbortSignal from intercepted fetch requests so datadome can show the challenge
+  this.ddResponsePage = "origin"; // where the challenge page comes from ("origin"/"datadome")
+  this.isSalesforce = false; // special mode for Salesforce Commerce Cloud (uses x-sf-cc-x-dd-b header instead of x-dd-b)
+  this.disableAutoRefreshOnCaptchaPassed = false; // if true, doesn't refresh automatically after the captcha resolution
+  this.enableTagEvents = false; // enable custom events (to listen to datadome actions like dd_ready, dd_blocked, dd_captcha_passed...)
+  this.withCredentials = false; // if true => include cookies in cors
+  this.overrideCookieDomain = false; // lets you define a custom domain for the datadome cookie
+  this.dryRun = []; // array of module IDs to disable (1=sync, 2=async, 3=events, 4=async challenges, 5=detection flag)
+  this.sessionByHeader = false; // if true, transmits the session via http header instead of cookie
+  this.ddCookieSessionName = "ddSession"; // name of the datadome session cookie
+  this.enableServiceWorkerPlugin = false;  // enable service worker
+  this.deferSignals = false; // if true, waits for window.load before launching modules
+  this.replayAfterChallenge = false; // if true, automatically replays the blocked request after challenge resolution
+  this.enableCookieDomainFallback = false;  // if true, use an alternative cookie domain if the main one fails 
+  this.challengeLanguage = null; // force language for the captcha (fr,en...)
+
+  // merge user options (window.ddoptions) onto the defaults
+  this.check = function (userOptions) {
+    if (!userOptions || typeof userOptions != "object") {
+      userOptions = {};
     }
-    if (n.ajaxListenerPath == null && window.ddCaptchaOptions == null) {
-      n.ajaxListenerPath = true;
+    if (userOptions.ajaxListenerPath == null && window.ddCaptchaOptions == null) {
+      userOptions.ajaxListenerPath = true;
     }
-    this.endpoint = function (n) {
-      if (n) {
-        return n;
-      }
-      var e = "https://api-js.datadome.co/js/";
-      var t = "https://js.datadome.co/";
-      var r = document && document.currentScript ? document.currentScript.src : t;
-      if (r.indexOf("https://js.datadome.co/") === 0) {
-        return e;
-      }
-      if (r.indexOf("/tags.js") !== -1) {
-        return r.replace("/tags.js", "/js/");
-      } else {
-        return r;
-      }
-    }(n.endpoint);
-    function e(n) {
-      var e = null;
-      var t = typeof n;
-      if (t !== "undefined") {
-        var r = n;
-        if (t === "string") {
-          e = [{
-            url: r
-          }];
-        } else if (r === true) {
-          e = [{
-            url: document.location.host
-          }];
-        } else if (Array.isArray(r)) {
-          if (r.length > 0) {
-            e = [];
-            for (var i = 0; i < r.length; ++i) {
-              var c = r[i];
-              var o = typeof c;
-              if (o === "string") {
-                e.push({
-                  url: c
-                });
-              } else if (o === "object") {
-                e.push(c);
-              }
-            }
-          }
-        } else if (t === "object") {
-          e = [r];
-        }
-      }
-      return e;
-    }
-    this.ajaxListenerPath = e(n.ajaxListenerPath);
-    this.ajaxListenerPathExclusion = e(n.ajaxListenerPathExclusion);
+
+    // get api endpoint from script url or the user config
+    this.endpoint = resolveEndpoint(userOptions.endpoint);
+
+    // normalize paths into [{url: ...}]
+    this.ajaxListenerPath = normalizePath(userOptions.ajaxListenerPath);
+    this.ajaxListenerPathExclusion = normalizePath(userOptions.ajaxListenerPathExclusion);
     if (this.ajaxListenerPathExclusion == null) {
-      this.ajaxListenerPathExclusion = [{
-        url: "https://www.google-analytics.com"
-      }];
+      this.ajaxListenerPathExclusion = [{ url: "https://www.google-analytics.com" }];
     }
-    if (n.sfcc != null) {
-      this.isSalesforce = n.sfcc;
-    }
-    if (n.customParam != null) {
-      this.customParam = n.customParam;
-    }
-    if (n.exposeCaptchaFunction != null) {
-      this.exposeCaptchaFunction = n.exposeCaptchaFunction;
-    }
-    if (n.abortAsyncOnCaptchaDisplay != null) {
-      this.abortAsyncOnChallengeDisplay = n.abortAsyncOnCaptchaDisplay;
-    }
-    if (n.abortAsyncOnChallengeDisplay != null) {
-      this.abortAsyncOnChallengeDisplay = n.abortAsyncOnChallengeDisplay;
-    }
-    if (n.debug != null) {
-      this.debug = n.debug;
-    }
-    if (n.testingMode != null) {
-      this.testingMode = n.testingMode;
-    }
-    if (n.eventsTrackingEnabled != null) {
-      this.eventsTrackingEnabled = n.eventsTrackingEnabled;
-    }
-    if (n.responsePage != null) {
-      this.ddResponsePage = n.responsePage;
-    }
-    if (n.patternToRemoveFromReferrerUrl != null) {
-      this.patternToRemoveFromReferrerUrl = n.patternToRemoveFromReferrerUrl;
-    }
-    if (n.overrideAbortFetch != null) {
-      this.overrideAbortFetch = n.overrideAbortFetch;
-    }
-    if (n.disableAutoRefreshOnCaptchaPassed != null) {
-      this.disableAutoRefreshOnCaptchaPassed = n.disableAutoRefreshOnCaptchaPassed;
-    }
-    if (n.enableTagEvents != null) {
-      this.enableTagEvents = n.enableTagEvents;
-    }
-    if (n.withCredentials != null) {
-      this.withCredentials = n.withCredentials;
-    }
-    if (n.overrideCookieDomain != null) {
-      this.overrideCookieDomain = n.overrideCookieDomain;
-    }
-    if (n.dryRun != null) {
-      this.dryRun = n.dryRun;
-    }
-    if (n.sessionByHeader != null) {
-      this.sessionByHeader = n.sessionByHeader;
-      window.ddSbh = n.sessionByHeader;
-      if (n.cookieName != null && n.cookieName != "") {
-        this.ddCookieSessionName = "ddSession_" + n.cookieName;
+
+    // copy user options -> defaults
+    if (userOptions.sfcc != null) this.isSalesforce = userOptions.sfcc;
+    if (userOptions.customParam != null) this.customParam = userOptions.customParam;
+    if (userOptions.exposeCaptchaFunction != null) this.exposeCaptchaFunction = userOptions.exposeCaptchaFunction;
+    if (userOptions.abortAsyncOnCaptchaDisplay != null) this.abortAsyncOnChallengeDisplay = userOptions.abortAsyncOnCaptchaDisplay;
+    if (userOptions.abortAsyncOnChallengeDisplay != null) this.abortAsyncOnChallengeDisplay = userOptions.abortAsyncOnChallengeDisplay;
+    if (userOptions.debug != null) this.debug = userOptions.debug;
+    if (userOptions.testingMode != null) this.testingMode = userOptions.testingMode;
+    if (userOptions.eventsTrackingEnabled != null) this.eventsTrackingEnabled = userOptions.eventsTrackingEnabled;
+    if (userOptions.responsePage != null) this.ddResponsePage = userOptions.responsePage;
+    if (userOptions.patternToRemoveFromReferrerUrl != null) this.patternToRemoveFromReferrerUrl = userOptions.patternToRemoveFromReferrerUrl;
+    if (userOptions.overrideAbortFetch != null) this.overrideAbortFetch = userOptions.overrideAbortFetch;
+    if (userOptions.disableAutoRefreshOnCaptchaPassed != null) this.disableAutoRefreshOnCaptchaPassed = userOptions.disableAutoRefreshOnCaptchaPassed;
+    if (userOptions.enableTagEvents != null) this.enableTagEvents = userOptions.enableTagEvents;
+    if (userOptions.withCredentials != null) this.withCredentials = userOptions.withCredentials;
+    if (userOptions.overrideCookieDomain != null) this.overrideCookieDomain = userOptions.overrideCookieDomain;
+    if (userOptions.dryRun != null) this.dryRun = userOptions.dryRun;
+    if (userOptions.sessionByHeader != null) {
+      this.sessionByHeader = userOptions.sessionByHeader;
+      window.ddSbh = userOptions.sessionByHeader;
+      if (userOptions.cookieName != null && userOptions.cookieName != "") {
+        this.ddCookieSessionName = "ddSession_" + userOptions.cookieName;
       }
     }
-    if (n.enableServiceWorkerPlugin != null) {
-      this.enableServiceWorkerPlugin = n.enableServiceWorkerPlugin;
-    }
-    if (n.deferSignals != null) {
-      this.deferSignals = n.deferSignals;
-    }
-    if (n.replayAfterChallenge === true) {
-      this.replayAfterChallenge = n.replayAfterChallenge;
+    if (userOptions.enableServiceWorkerPlugin != null) this.enableServiceWorkerPlugin = userOptions.enableServiceWorkerPlugin;
+    if (userOptions.deferSignals != null) this.deferSignals = userOptions.deferSignals;
+    if (userOptions.replayAfterChallenge === true) {
+      this.replayAfterChallenge = userOptions.replayAfterChallenge;
       this.disableAutoRefreshOnCaptchaPassed = true;
     }
-    if (typeof n.enableCookieDomainFallback == "boolean") {
-      this.enableCookieDomainFallback = n.enableCookieDomainFallback;
-    }
-    if (typeof n.challengeLanguage == "string") {
-      this.challengeLanguage = n.challengeLanguage;
-    }
+    if (typeof userOptions.enableCookieDomainFallback == "boolean") this.enableCookieDomainFallback = userOptions.enableCookieDomainFallback;
+    if (typeof userOptions.challengeLanguage == "string") this.challengeLanguage = userOptions.challengeLanguage;
   };
 };
+
+// get API endpoint from the datadome script url
+// - if user set a custom endpoint => returns it
+// - if loaded from https://js.datadome.co/tags.js => returns https://api-js.datadome.co/js/
+// - if self-hosted like https://mysite.com/datadome/tags.js => returns https://mysite.com/datadome/js/
+// - otherwise => returns the script url as-is
+function resolveEndpoint(userEndpoint) {
+  if (userEndpoint) return userEndpoint;
+  
+  var defaultApi = "https://api-js.datadome.co/js/";
+  var defaultCdn = "https://js.datadome.co/";
+  var scriptSrc = document && document.currentScript ? document.currentScript.src : defaultCdn;
+
+  if (scriptSrc.indexOf(defaultCdn) === 0) return defaultApi;
+  if (scriptSrc.indexOf("/tags.js") !== -1) return scriptSrc.replace("/tags.js", "/js/");
+  
+  return scriptSrc;
+}
+
+// normalize into [{url: ...}] :
+// "example.com"  -> [{url: "example.com"}]
+// true           -> [{url: document.location.host}]
+// [{url:..}]     -> [{url:..}]
+// {url:..}       -> [{url:..}]
+function normalizePath(value) {
+    if (typeof value === "undefined") return null;
+    if (typeof value === "string") return [{ url: value }];
+    if (value === true) return [{ url: document.location.host }];
+
+    if (Array.isArray(value)) {
+      if (value.length === 0) return null;
+      var result = [];
+
+      for (var i = 0; i < value.length; i++) {
+        var item = value[i];
+        if (typeof item === "string") result.push({ url: item });
+        else if (typeof item === "object") result.push(item);
+      }
+
+      return result;
+    }
+
+    if (typeof value === "object") return [value];
+
+    return null;
+}
